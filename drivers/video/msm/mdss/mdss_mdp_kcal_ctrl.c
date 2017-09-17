0c0dd9fa9b850f463cd1ca1aef0e26d2080f8fcf
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2017-09-17 12:19:39 +0000

    Remove invert screen from KCAL

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -246,33 +246,6 @@ static ssize_t kcal_enable_show(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%d\n", lut_data->enable);
 }
 
-static ssize_t kcal_invert_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
-{
-	int kcal_invert, r;
-	struct kcal_lut_data *lut_data = dev_get_drvdata(dev);
-
-	r = kstrtoint(buf, 10, &kcal_invert);
-	if ((r) || (kcal_invert != 0 && kcal_invert != 1) ||
-		(lut_data->invert == kcal_invert))
-		return -EINVAL;
-
-	lut_data->invert = kcal_invert;
-
-	mdss_mdp_kcal_update_pcc(lut_data);
-	mdss_mdp_kcal_display_commit();
-
-	return count;
-}
-
-static ssize_t kcal_invert_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct kcal_lut_data *lut_data = dev_get_drvdata(dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", lut_data->invert);
-}
-
 static ssize_t kcal_sat_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
@@ -381,8 +354,6 @@ static DEVICE_ATTR(kcal, S_IWUSR | S_IRUGO, kcal_show, kcal_store);
 static DEVICE_ATTR(kcal_min, S_IWUSR | S_IRUGO, kcal_min_show, kcal_min_store);
 static DEVICE_ATTR(kcal_enable, S_IWUSR | S_IRUGO, kcal_enable_show,
 	kcal_enable_store);
-static DEVICE_ATTR(kcal_invert, S_IWUSR | S_IRUGO, kcal_invert_show,
-	kcal_invert_store);
 static DEVICE_ATTR(kcal_sat, S_IWUSR | S_IRUGO, kcal_sat_show, kcal_sat_store);
 static DEVICE_ATTR(kcal_hue, S_IWUSR | S_IRUGO, kcal_hue_show, kcal_hue_store);
 static DEVICE_ATTR(kcal_val, S_IWUSR | S_IRUGO, kcal_val_show, kcal_val_store);
