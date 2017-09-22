d2cd3b0078023bb39f5d8531b12bfd46946b8164
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2017-09-22 10:18:39 +0000

    Some more wakelock togglez

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -23,6 +23,8 @@ static bool enable_wlan_ctrl_wake_ws = true;
 module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
 static bool enable_wlan_wake_ws = true;
 module_param(enable_wlan_wake_ws, bool, 0644);
+static bool enable_msm_hsic_ws = true;
+module_param(enable_msm_hsic_ws, bool, 0644);
 
 #include "power.h"
 
@@ -427,6 +429,11 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 
 	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
                 return;  
+
+        if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) {
+                pr_info("wakeup source msm_hsic_host activate skipped\n");
+                return;
+        }
 
 	/*
 	 * active wakeup source should bring the system
